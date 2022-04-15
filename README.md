5a78e3ebb3b41a92580330b7402ccb5820ecb75a
EliasScharlach <elias.scharlach@icloud.com>
2022-04-15 20:13:12 +0200

    changed website for testing

--- README.md
+++
@@ -1,1 +0,0 @@
-# EliasSchar.github.io
---
+++ precache-manifest.2dc9cb3b31a60e288d6de1715cbf1626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c132a0748938b24873f3",
+    "url": "/static/js/main.c132a074.chunk.js"
+  },
+  {
+    "revision": "cefe3c6a8e52e56215da",
+    "url": "/static/js/1.cefe3c6a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fe558130b5558600ead52164f4a34bc",
+    "url": "/static/media/WebsiteOwner.2fe55813.png"
+  },
+  {
+    "revision": "9168414acd9cbf8b2310a7fe397f0b8f",
+    "url": "/static/media/WebsiteOwner_old.9168414a.png"
+  },
+  {
+    "revision": "a992985eba1a6dcf7a13966f78f0d527",
+    "url": "/index.html"
+  }
+];
